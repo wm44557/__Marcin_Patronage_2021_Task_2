7323b9d4a55fc40da3e3bce195beba1e02f18424
Marcin Wrzos <marwrz36@gmail.com>
2021-02-16 18:38:43 +0100

    Add form with script and fix gradient

--- add.js
+++ add.js
@@ -3,9 +3,9 @@ import MoviesStorage from "./movies-storage.js";
 import {moviesCounter, moviesSeenCounter} from "./movies-counter.js";
 
 const dElement = {
-    moviesSeen: document.getElementById('anotherMoviesCounterAll'),
-    moviesCounter: document.getElementById('anotherMoviesCounterSeen'),
-    moviesContainer: document.getElementById('formContainer')
+    moviesSeen: document.getElementById('anotherMoviesCounterSeen'),
+    moviesCounter: document.getElementById('anotherMoviesCounterAll'),
+    container: document.getElementById('formContainer')
 }
 
 let store = new MoviesStorage();
@@ -17,5 +17,30 @@ setCounterOfTo(moviesSeenCounter, moviesCounterSeenFn(), 'MoviesSeen')
 dElement['moviesSeen'].textContent = String(moviesSeenCounter);
 dElement['moviesCounter'].textContent = String(moviesCounter);
 
+const addForm = () => {
+    const form = document.createElement('form');
+    let itemChildListItem;
 
+    const appendChild = (tagName, innerHTML, placeholder, listener, type) => {
+        itemChildListItem = document.createElement(tagName)
+        itemChildListItem.innerHTML = innerHTML;
+        placeholder ? itemChildListItem.placeholder = placeholder : null;
+        type ? itemChildListItem.type = type : null;
+
+
+        form.appendChild(itemChildListItem);
+    }
+
+    appendChild('h1', 'Add film');
+    appendChild('input', '', 'title', 'change', 'text');
+    appendChild('input', '', 'year', 'change', 'number');
+    appendChild('input', '', 'year', 'change', 'text');
+    appendChild('input', '', 'summary', 'change', 'text');
+    appendChild('p', '');
+    appendChild('button', 'SAVE', null, 'click');
+    dElement['container'].appendChild(form);
+}
+
+
+addForm()
 
